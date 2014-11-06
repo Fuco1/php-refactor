76a9ee5052cd5ca8e0596f550864eb537fb17ccf
Matus Goljer <dota.keys@gmail.com>
2014-11-06 11:02:21 +0100

    Add elisp

---
+++ php-refactor.el
@@ -0,0 +1,31 @@
+;;; php-refactor.el --- Refactoring library for php
+
+;; Copyright (C) 2014 Matúš Goljer <matus.goljer@gmail.com>
+
+;; Author: Matúš Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
+;; Version: 0.0.1
+;; Created: 6th November 2014
+;; Package-requires: ((dash "2.9.0"))
+;; Keywords: languages, convenience
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+(require 'dash)
+
+(provide 'php-refactor)
+;;; php-refactor.el ends here
