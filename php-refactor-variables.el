582cc0ab57aca5bda0c9c279d7b97e4808a17102
Matus Goljer <matus.goljer@gmail.com>
2016-03-08 22:41:27 +0100

    Add basic elisp to work with variables

---
+++ php-refactor-variables.el
@@ -0,0 +1,44 @@
+;;; php-refactor-variables.el --- Refactoring library for php
+
+;; Copyright (C) 2016 Matúš Goljer
+
+;; Author: Matúš Goljer <matus.goljer@gmail.com>
+;; Maintainer: Matúš Goljer <matus.goljer@gmail.com>
+;; Version: 0.0.1
+;; Created: 8th March 2016
+;; Package-requires: ((dash "2.12.0"))
+;; Keywords: languages, convenience
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 3
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'json)
+
+(defun php-refactor-get-variables (file)
+  "Get variables."
+  (with-temp-buffer
+    (call-process
+     "php" nil (current-buffer) nil
+     "/home/matus/.emacs.d/projects/php-refactor/bin/parser" "variables" file)
+    (pop-to-buffer (current-buffer))
+    (goto-char (point-min))
+    (json-read)))
+
+(php-refactor-get-variables "./php/example2.php")
+
+(provide 'php-refactor-variables)
+;;; php-refactor-variables.el ends here
