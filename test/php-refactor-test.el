c0b45d018efee3b7177fc1d68e384673709211cd
Matus Goljer <matus.goljer@gmail.com>
2016-03-20 12:29:34 +0100

    More renaming tests

--- test/php-refactor-test.el
+++ test/php-refactor-test.el
@@ -1,18 +1,50 @@
+(defun php-refactor-test--setup-buffer ()
+  "Setup the test buffer."
+  (php-mode)
+  (delete-selection-mode 1)
+  (transient-mark-mode 1))
+
 (ert-deftest php-refactor-test-rename-variable ()
   (let ((initial "<?php
 $foo = 1;
 $bar = $f|oo + $a;
 return $foo;"))
     (php-refactor-test-with-temp-buffer initial
-        (progn
-          (php-mode)
-          (delete-selection-mode 1)
-          (transient-mark-mode 1))
+        (php-refactor-test--setup-buffer)
       (php-refactor-rename-variable)
-      ;; (call-interactively 'kill-region)
       (execute-kbd-macro "baz")
       (insert "|")
       (should (equal (buffer-string) "<?php
 $baz = 1;
 $bar = $baz| + $a;
 return $baz;")))))
+
+(ert-deftest php-refactor-test-rename-variable-outside-nested-function ()
+  (let ((initial "<?php
+$f|oo = 1;
+$bar = function ($foo) { return $foo + $a; };
+return $foo;"))
+    (php-refactor-test-with-temp-buffer initial
+        (php-refactor-test--setup-buffer)
+      (php-refactor-rename-variable)
+      (execute-kbd-macro "baz")
+      (insert "|")
+      (should (equal (buffer-string) "<?php
+$baz| = 1;
+$bar = function ($foo) { return $foo + $a; };
+return $baz;")))))
+
+(ert-deftest php-refactor-test-rename-variable-inside-nested-function ()
+  (let ((initial "<?php
+$foo = 1;
+$bar = function ($fo|o) { return $foo + $a; };
+return $foo;"))
+    (php-refactor-test-with-temp-buffer initial
+        (php-refactor-test--setup-buffer)
+      (php-refactor-rename-variable)
+      (execute-kbd-macro "baz")
+      (insert "|")
+      (should (equal (buffer-string) "<?php
+$foo = 1;
+$bar = function ($baz|) { return $baz + $a; };
+return $foo;")))))
